b962dd0e48e5d0e0314ff915a7090de434a20969
Andrew Uhlian <acuhlian@umich.edu>
2025-02-20 15:27:11 -0500

    integrated components directory into Homepage

--- src/app/Homepage.tsx
+++ src/app/Homepage.tsx
@@ -3,6 +3,8 @@ import { User } from "firebase/auth";
 import { db } from "../../firebaseConfig";
 import { collection, addDoc, getDocs } from "firebase/firestore";
 import { Star } from "lucide-react"; // to change star icons
+import Navbar from "@/components/Navbar";
+import StarRating from "@/components/StarRating";
 
 interface Review {
   id: string;
@@ -87,42 +89,6 @@ export default function HomePage({ user, handleLogout }: { user: User; handleLog
     return `${selectedBuilding} - ${selectedBathroom === "Other" ? customBathroom : selectedBathroom}`;
   };
 
-  // Define star rating props type for rating function
-  interface StarRatingProps {
-    rating: number | null;
-    setRating: (rating: number) => void;
-  }
-  
-  // Star rating function component
-  // Handles hovering and shading status of star inputs on rating
-  const StarRating: React.FC<StarRatingProps> = ({ rating, setRating }) => {
-    const [hovered, setHovered] = useState<number | null>(null);
-    return (
-      <div className="flex space-x-2 mb-4">
-        {// 5 stars, map each star to some val num
-        // when hovering highlight star and prior
-        // When clicked, select num for rating and stop
-        // hovering feature
-        }
-        {[1, 2, 3, 4, 5].map((num) => (
-          <button
-            key={num}
-            onClick={() => setRating(num)}
-            onMouseEnter={() => setHovered(num)}
-            onMouseLeave={() => setHovered(null)}
-            className="text-yellow-500 text-3xl cursor-pointer transition duration-200"
-          >
-            <Star
-              size={32}
-              fill={num <= (hovered ?? rating ?? 0) ? "gold" : "none"}
-              stroke="gold"
-            />
-          </button>
-        ))}
-      </div>
-    );
-  };
-
   
   const handleSubmitReview = async () => {
     const location = getLocationString();
@@ -175,7 +141,7 @@ export default function HomePage({ user, handleLogout }: { user: User; handleLog
       >
         Logout
       </button>
-
+    <Navbar/>
       <div className="mb-6">
         <h3 className="text-xl font-semibold mb-4">Leave a Restroom Review</h3>
         
